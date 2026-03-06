92dc753d97a33adad9ecf081c270a41a310dbd10
devwilson <ohioleomole@gmail.com>
2026-03-06 19:01:50 +0000

    added some pages

---
+++ src/app/auth.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const auth = () => {
+  return (
+    <View>
+      <Text>auth</Text>
+    </View>
+  );
+};
+
+export default auth;
+
+const styles = StyleSheet.create({});
