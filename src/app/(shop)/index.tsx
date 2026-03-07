688c85d14a4ca0c0db8f7126b1287b17d87ef2e3
devwilson <ohioleomole@gmail.com>
2026-03-07 18:47:10 +0000

    render mocked product list

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { PRODUCTS } from "../../../assets/products";
+import { ProductListItem } from "@/components/ProductListItem";
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListEmptyComponent={<Text>Products</Text>}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flatListContent: { paddingBottom: 20 },
+  flatListColumn: { justifyContent: "space-between" },
+});
