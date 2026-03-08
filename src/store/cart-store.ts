18518347d41f6b41cb4a975d13c8ae28241d11c2
devwilson <ohioleomole@gmail.com>
2026-03-08 18:46:45 +0000

    add cart store logic

---
+++ src/store/cart-store.ts
@@ -0,0 +1,84 @@
+import { create } from "zustand";
+import { PRODUCTS } from "../../assets/products";
+
+type CartItemType = {
+  id: number;
+  title: string;
+  heroImage: string;
+  price: number;
+  quantity: number;
+  maxQuantity: number;
+};
+
+type CartState = {
+  items: CartItemType[];
+  addItem: (item: CartItemType) => void;
+  removeItem: (id: number) => void;
+  incrementItem: (id: number) => void;
+  decrementItem: (id: number) => void;
+  getTotalPrice: () => string;
+  getItemCount: () => number;
+  resetCart: () => void;
+};
+
+const initialCartItems: CartItemType[] = [];
+
+export const useCartStore = create<CartState>((set, get) => ({
+  items: initialCartItems,
+  addItem: (item: CartItemType) => {
+    const existingItem = get().items.find((item) => item.id === item.id);
+    if (existingItem) {
+      set((state) => ({
+        items: state.items.map((itemExist) =>
+          itemExist.id === item.id
+            ? {
+                ...itemExist,
+                quantity: Math.min(
+                  itemExist.quantity + item.quantity,
+                  PRODUCTS.find((product) => product.id === item.id)
+                    ?.maxQuantity || 0,
+                ),
+              }
+            : itemExist,
+        ),
+      }));
+    } else {
+      set((state) => ({ items: [...state.items, item] }));
+    }
+  },
+  removeItem: (id: number) =>
+    set((state) => ({ items: state.items.filter((item) => item.id !== id) })),
+  incrementItem: (id: number) =>
+    set((state) => {
+      const product = PRODUCTS.find((product) => product.id === id);
+      if (!product) return state;
+
+      return {
+        items: state.items.map((item) =>
+          item.id === id && item.quantity < item.maxQuantity
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        ),
+      };
+    }),
+  decrementItem: (id: number) =>
+    set((state) => ({
+      items: state.items.map((item) =>
+        item.id === id && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item,
+      ),
+    })),
+  getTotalPrice: () => {
+    const { items } = get();
+
+    return items
+      .reduce((total, item) => total + item.price * item.quantity, 0)
+      .toFixed(2);
+  },
+  getItemCount: () => {
+    const { items } = get();
+    return items.reduce((count, item) => count + item.quantity, 0);
+  },
+  resetCart: () => set({ items: initialCartItems }),
+}));
