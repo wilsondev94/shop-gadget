6d83eb01f27c1500b438ab0e36b9ebdf97dbc0fd
devwilson <ohioleomole@gmail.com>
2026-03-11 17:42:38 +0000

    add content to orders page

--- assets/types/order.ts
+++ assets/types/order.ts
@@ -1,6 +1,6 @@
-import { Product } from './product';
+import { Product } from "./product";
 
-export type OrderStatus = 'Pending' | 'Completed' | 'Shipped' | 'InTransit';
+export type OrderStatus = "Pending" | "Completed" | "Shipped" | "InTransit";
 
 export type Order = {
   id: string;
@@ -9,5 +9,5 @@ export type Order = {
   details: string;
   status: OrderStatus;
   date: string;
-  items: Product[];
+  items: Omit<Product, "category" | "maxQuantity">[];
 };
